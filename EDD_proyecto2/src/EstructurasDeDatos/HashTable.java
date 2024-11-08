1ad881be84b0dff8a0c338db4657ed465e373982
Santiagocm15 <157600349+Santiagocm15@users.noreply.github.com>
2024-11-08 16:14:50 -0400

    Add files via upload

---
+++ EDD_proyecto2/src/EstructurasDeDatos/HashTable.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EstructurasDeDatos;
+
+/**
+ *
+ * @author Michelle García
+ */
+public class HashTable {
+
+    private int capacity;
+    private List nodes;
+    private List[] table;
+
+    // Utiliza el método 
+    public int hash(String name) {
+        final int FNV_PRIME = 0x811C9DC5;
+        final int OFFSET_BASIS = 0x1000193;
+
+        int hash = OFFSET_BASIS;
+        for (int i = 0; i < name.length(); i++) {
+            hash ^= name.charAt(i);
+            hash *= FNV_PRIME;
+        }
+        return hash;
+    }
+
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        this.nodes = nodes;
+        this.table = new List[capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            table[i] = new List();
+        }
+    }
+
+    // Eliminar dentro del hashtable
+    public void delete(Node father) {
+
+    }
+}
